49e3fdde123635cefdc7d8eba3e9d506eb4ae9b0
Unah Michael <unahmichael76@gmail.com>
2023-09-08 01:50:03 -0700

    Hello Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This serves as a starting point for program execution
+ *
+ * Return: always takes the value of 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

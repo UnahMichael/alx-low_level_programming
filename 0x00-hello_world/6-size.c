8fa646497cfe8d57b74a75d03c8f7b02f75592fc
Unah Michael <unahmichael76@gmail.com>
2023-09-08 03:12:03 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - This is serves as a starting point for program execution
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("size of char: %lu byte(s)", sizeof(char);
+	printf("size of int: %lu byte(s)", sizeof(int));
+	printf("size of a long: %lu byte(s)", sizeof(long int));
+	printf("size of a long long: %lu byte(s)", sizeof(long long int));
+	printf("size of a float: %lu byte(s)", sizeof(float));
+	return (0);
+}

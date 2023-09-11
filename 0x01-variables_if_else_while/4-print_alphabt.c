0ff6e34e69ccb6517aaba13303170e58557e1ced
Unah Michael <unahmichael76@gmail.com>
2023-09-11 15:31:23 -0700

    Alphabet1

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+
+	for (i = 'a' ; i <= 'z' ; i++)
+
+	if (i != 'q' && i != 'e')
+
+	putchar('\n');
+
+	return (0);

52bd0b6d3c5ea38edc4da8309fcfba9d2fa714c5
Unah Michael Thankgod <unahmichael76@gmail.com>
2023-11-01 17:38:02 -0700

    Hello World

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments
+ * @argc: arguments counts
+ * @argv: arguments vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
